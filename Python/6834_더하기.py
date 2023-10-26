869846884da90d685f319d033156619e49b32d7c
ryu <munam4@naver.com>
2023-10-26 19:36:11 +0900

    231026 2주차 - 더하기.py

--- Python/6834_더하기.py
+++ Python/6834_더하기.py
@@ -1,30 +1,32 @@
 
-def find(sum):
+def find(sum, idx):
+    
     global flag
     if flag == True:
         return
-    if sum > K:
+    if idx == N+1:
         return
-    if sum == K:
+    if sum == 0:
         flag = True
         return
+    if sum > hab[N] - hab[idx-1]:
+        return
     
-    for i in range(N):
-        if visited[i] == 1:
-            continue
-        visited[i] = 1
-        find(sum+d[i])
-        visited[i] = 0
-
+    for i in range(idx, N):
+        find(sum-A[i],i+1)
 
 t = int(input())
 for _ in range(t):
     N,K = map(int,input().split())
-    d = list(map(int,input().split()))
+    A = list(map(int,input().split()))
+    hab = [0] * (N)
     visited = [0] * N
-
+    hab[0] = A[0]
+    for i in range(1,N):
+        hab[i] = hab[i-1] + A[i]
+    hab = [0] + hab
     flag = False
-    find(0)
+    find(K,0)
     if flag:
         print("YES")
     else:
