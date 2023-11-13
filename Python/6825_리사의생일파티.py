0fa8a20735c2841b6fd3a35756290359051f9b8e
ryu <munam4@naver.com>
2023-11-13 21:46:12 +0900

    231113 4주차 - 리사의생일파티(python, 수정)

--- Python/6825_리사의생일파티.py
+++ Python/6825_리사의생일파티.py
@@ -1,38 +1,74 @@
 
-# 이분탐색? 아닌데.
 # 만들 수 있는 개수를 갱신해 가면서 만들어 나가기..?
-# DFS?
-
-
-def DFS(total):
-    if total > M:
-        return
-    
-    possible_make = 50000000
-    global maxV
-    for i in range(N):
-        possible_make = min(possible_make, having_cnt[i] // info[i][0])
-    
-    if maxV < possible_make:
-        maxV = possible_make
-
-    for i in range(N):
-        for j in range(2):
-            # j = 0 -> 작은 포장, j = 1 -> 큰 포장 구매
-            # 구매시 가능한 식사 준비량 넘기기.
-            buy_cnt[i][j] += 1
-            having_cnt[i] += info[i][j*2 + 2]
-            DFS(total + info[i][j*2+3])
-            buy_cnt[i][j] -= 1
-            having_cnt[i] -= info[i][j*2 + 2]
+# DFS? 시간초과
+# 이분탐색 -> 만들 수 있는 개수를 탐색하자
+# 만들 수 있는 개수 -> 1 ~ 100000
 
 N,M = map(int,input().split())
-info = [list(map(int,input().split())) for _ in range(N)]
-buy_cnt = [[0,0] for _ in range(N)]
-having_cnt = [0] * N
-for i in range(N):
-    having_cnt[i] = info[i][1]
+X = [0] * 101
+Y = [0] * 101
+Sm = [0] * 101
+Pm = [0] * 101
+Sv = [0] * 101
+Pv = [0] * 101
 
-maxV = 0
-DFS(0)
-print(maxV)
+for i in range(N):
+    X[i], Y[i], Sm[i], Pm[i], Sv[i], Pv[i] = map(int,input().split())
+
+result = 0
+
+
+l = 0
+r = 100000
+
+while l <= r:
+    # middle : 만들 수 있는 개수
+    middle = (r + l) // 2
+
+    need_money = 0
+
+    # 필요한 양을 살 수 있는 최소 비용? 작은애들 먼저? 
+    # 반복문 -> 작은 애들로만 채웠을때 -> 작은 애들을 하나씩 줄이면서 큰 애들을 넣어주기?
+    # 모든 재료들이 middle개 만들 수 있는 상태가 되어야한다.
+    # Y + Yplus // X[i] == 50000
+    # 8 + x // 10 == 50000
+    # x = middel * X[i] - Y[i]
+    
+    # 작은 팩과 큰 팩을 사는 것 중 어느 것을 사는게 더 이득인가?
+    for i in range(N):
+        need = middle * X[i] - Y[i]
+        min_money = 1000000
+        # 작은 팩으로 하나씩 늘려가면서, 큰 팩으로 몇개 사야하는지 확인.
+
+        # middle = 23 만들어야함
+        # need = 222개
+        # small의 범위 -> 0 ~ 23
+        # temp_money = 
+
+        
+        for small in range(need // Sm[i] + 1):
+            temp_money = small * Pm[i]
+            more_need = need - Sm[i] * small
+            big = more_need // Sv[i]
+
+            if more_need % Sv[i]:
+                big += 1
+            
+            temp_money += big * Pv[i]
+
+            if min_money > temp_money:
+                min_money = temp_money
+
+        need_money += min_money
+    
+    # middle만큼 만드는데 필요한 돈이 현재 가지고 있는 금액보다 크다면 -> r 하향
+    # 작다면 -> l 상향, result 갱신
+    if need_money > M:
+        r = middle - 1
+    elif need_money < M:
+        l = middle + 1
+        result = middle
+    else:
+        result = middle
+        break
+print(result)
